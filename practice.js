75d7f0be2403a05dcb861f50b21a17be1fb4b825
KushalAbhishek <abhishek120340@gmail.com>
2025-09-06 12:33:46 +0530

    datatypes in js

--- practice.js
+++ practice.js
@@ -1,1 +1,35 @@
 console.log("Cyberpunk 2077")
+
+let name = "Kushal Abhishek"
+console.log(name)
+
+let num = 27;
+console.log(num)
+
+let x
+console.log(x)
+
+let y = null
+console.log(y)
+
+let isLogged = false
+console.log(isLogged)
+
+console.log(typeof name)
+console.log(typeof num)
+console.log(typeof isLogged)
+console.log(typeof x)
+console.log(typeof y)
+
+let arr = [1,2,3,4,5]
+console.log(arr[0])
+console.log(typeof arr)
+
+let person = {
+  name: "Abhishek",
+  age: 21
+}
+
+console.log(person.name)
+console.log(person.age)
+console.log(typeof person)
--- sample.js
+++ sample.js
@@ -1,1 +1,2 @@
 console.log("RED DEAD REDEMPTION 2")
+
